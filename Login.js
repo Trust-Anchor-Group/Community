b2bb428ffe8198329b17b495edc10a51bb162fba
Peter Waher <peterwaher@hotmail.com>
2022-12-25 13:17:07 -0300

    Fix: Login main page

--- Login.js
+++ Login.js
@@ -99,8 +99,13 @@ function SignatureReceivedBE(Empty)
 
     var s = window.location.href;
     var i = s.indexOf("from=");
-    s = s.substring(i + 5);
-    window.location.href = unescape(s);
+    if (i < 0)
+        window.location.href = "/Community/Index.md";
+    else
+    {
+        s = s.substring(i + 5);
+        window.location.href = unescape(s);
+    }
 }
 
 var LoginTimer = window.setTimeout(function () { DisplayQuickLogin(); }, 100);
